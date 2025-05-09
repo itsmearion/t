66e340c27d5920b4ccfe20350a6bf16cc0f8d2a9
Lampurge <bilqiss0893@gmail.com>
2025-05-10 00:00:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import openai
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
-from aiogram.dispatcher.storage import MemoryStorage
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
